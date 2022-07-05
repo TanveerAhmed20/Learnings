8dab6bb1cce5ed261adf0ffd5e75f9a9102bd399
tanveerahmed20 <tanveer.ahmed@brillio.com>
2022-07-05 11:16:57 +0530

    added brillo folder

---
+++ Brillio/tut1.js
@@ -0,0 +1,25 @@
+console.log('started ');
+
+var a = "12";
+var b = 12;
+var c = 12.5;
+
+console.log(parseInt(a)+b+c);
+console.log(a+b+c);
+
+//working with boolean 
+console.log(true+true);
+
+
+// null
+
+var a = null;
+var b = null;
+console.log(a+b);
+
+
+//undefined 
+
+var a = undefined;
+var b = undefined;
+console.log(a+b);
