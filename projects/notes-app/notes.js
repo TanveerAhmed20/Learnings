8dce5aa89f547d494486dd2a78f3eae1d57399a8
tanveerahmed20 <tanveer.ahmed@brillio.com>
2022-07-07 19:48:29 +0530

    update

---
+++ projects/notes-app/notes.js
@@ -0,0 +1,61 @@
+
+const fs = require('fs');
+
+const getNotes = () =>{
+    return 'Your notes ...';
+}
+
+const addNote = (title,body) =>{
+    const notes = loadNotes();
+
+    const duplicateNotes = notes.filter(x=>x.title ===title);
+    
+    if(duplicateNotes.length>0) { console.log("already existing notes"); return notes;}
+
+    notes.push({
+        title:title,
+        body : body
+    });
+    saveNotes(notes);
+    console.log('New node added ...');
+    console.log(notes);
+
+};
+
+//DELETE OPERATION
+const removeNote = (title)=>{
+    const notes = loadNotes();
+    if(notes.length === 0) { console.log("List is empty"); return; }
+    const found = notes.find(x=>x.title === title);
+    
+    if(found) {
+        const index= notes.indexOf(found,0);
+        // console.log("found at: " + index);
+        notes.splice(index,1);
+        console.log(notes);
+    }
+    else console.log("item not found");
+    return;
+}
+
+
+const saveNotes = (notes) =>{
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('./notes.json',dataJSON);
+};
+
+const loadNotes = () =>{
+    try{
+        const dataBuffer = fs.readFileSync('./notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    }
+    catch(e){
+        return [];
+    }
+}
+module.exports = {
+     getNotes:getNotes,
+     addNote :addNote,
+     removeNote : removeNote
+};
